ac95b3f13b208df6fa60e42f28b1c5e882f16f05
Mattia Fiumara <mfiumara@protonmail.com>
2023-09-13 00:43:49 +0200

    wip: add reset assert function

--- src/lib.rs
+++ src/lib.rs
@@ -13,31 +13,39 @@ use std::ptr;
 
 // Define your custom error type (Err)
 #[derive(Debug)]
-pub struct BmpError {
+pub struct BlackMagicProbeError {
     message: String,
 }
 
-impl fmt::Display for BmpError {
+impl fmt::Display for BlackMagicProbeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.message)
     }
 }
 
-impl Error for BmpError {}
+impl Error for BlackMagicProbeError {}
 /// A result type with the error hardwired to [`BmpError`].
-pub type Result<T> = std::result::Result<T, BmpError>;
-pub struct Bmp {
+pub type Result<T> = std::result::Result<T, BlackMagicProbeError>;
+
+#[derive(Debug)]
+pub struct BlackMagicProbe {
+    // The serial number of the probe
     serial: String,
+    // The maximum speed in Hz of the debug probe
+    pub speed: u32,
 }
 
-impl Bmp {
+const MAX_SPEED: u32 = 4_000_000;
+
+impl BlackMagicProbe {
+    // Opens the device handle for the specified serial number of the Black Magic Probe
     pub fn open_by_serial(serial: &str) -> Result<Self> {
         let input = CString::new(serial).expect("CString conversion failed");
         let options = bmda_cli_options {
             opt_target_dev: 1,
             opt_flash_size: 0xffffffff,
             opt_flash_start: 0xffffffff,
-            opt_max_swj_frequency: 4000000,
+            opt_max_swj_frequency: MAX_SPEED,
             opt_scanmode: bmp_scan_mode_BMP_SCAN_SWD,
             opt_mode: bmda_cli_mode_BMP_MODE_DEBUG,
 
@@ -58,23 +66,30 @@ impl Bmp {
         };
         let result = unsafe { serial_open(&options, input.as_ptr()) };
         if result == false {
-            return Err(BmpError {
+            return Err(BlackMagicProbeError {
                 message: "Could not open serial device".to_string(),
             });
         }
-        let bmp = Bmp {
+        let bmp = BlackMagicProbe {
             serial: serial.to_string(),
+            speed: MAX_SPEED,
         };
 
         let result = unsafe { remote_init(true) };
         if result == false {
-            return Err(BmpError {
+            return Err(BlackMagicProbeError {
                 message: "Could not initialize device".to_string(),
             });
         }
 
-        let ret: Result<Bmp> = Ok(bmp);
+        let ret: Result<BlackMagicProbe> = Ok(bmp);
         return ret;
+    }
+    // Asserts the nrst line when input is true
+    // Returns true or exits in case the call failed
+    pub fn nrst_set(assert: bool) -> Result<bool> {
+        unsafe { remote_nrst_set_val(assert) }
+        return Ok(true);
     }
 }
 
@@ -98,7 +113,7 @@ mod tests {
 
     #[test]
     fn test_bmp_open() {
-        let bmp = super::Bmp::open_by_serial("98B72495");
+        let bmp = super::BlackMagicProbe::open_by_serial("98B72495");
         match bmp {
             Ok(bmp) => {}
             Err(e) => {
